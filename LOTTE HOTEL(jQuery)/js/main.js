f352ad5288027bcebe5f0d1ae3ca0dc239410973
hyunsu1234 <cookirihy1@gmail.com>
2023-01-15 04:14:56 +0900

    updated

--- LOTTE HOTEL(jQuery)/js/main.js
+++ LOTTE HOTEL(jQuery)/js/main.js
@@ -1,41 +1,41 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    //팝업창
-    $(function () {
-        $(".btn_open").click(function () {
-            $(".popup_lists").toggleClass("on"),
-                $(".btn_open").toggleClass('on')
-        });
+//팝업창
+$(function () {
+    $(".btn_open").click(function () {
+        $(".popup_lists").toggleClass("on"),
+            $(".btn_open").toggleClass('on')
     });
+});
 
-    //header up down
-    $(function () {
-        var t = 0;
-        var timer = 0;
-        let ww = 0;
+//header up down
+$(function () {
+    var t = 0;
+    var timer = 0;
+    let ww = 0;
 
-        $(window).scroll(function () {
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                h = $('.main_img').offset().top
-                console.log(h);
-                t = $(window).scrollTop();
-                console.log(t);
-                ww = $(window).innerWidth();
-                if (t >= $('.main_img').offset().top, ww >= 768) {
-                    $(".top").slideUp(400);
-                }
-                if (t <= $('.main_img').offset().top) {
-                    $(".top").slideDown(400);
-                }
-            }, 500);
-        })
-    });
-
-    //quick 맨위로 이동
-    $(function () {
-        $('.quick').click(function () {
-            $('html, body').animate({ scrollTop: '0' }, 680);
-        });
+    $(window).scroll(function () {
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            h = $('.main_img').offset().top
+            console.log(h);
+            t = $(window).scrollTop();
+            console.log(t);
+            ww = $(window).innerWidth();
+            if (t >= $('.main_img').offset().top, ww >= 768) {
+                $(".top").slideUp(400);
+            }
+            if (t <= $('.main_img').offset().top) {
+                $(".top").slideDown(400);
+            }
+        }, 500);
     })
 });
+
+//quick 맨위로 이동
+$(function () {
+    $('.quick').click(function () {
+        $('html, body').animate({ scrollTop: '0' }, 680);
+    });
+})
+    // });
