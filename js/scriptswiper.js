64a34a2caab1e0eaa88df346f3eccb2577e73273
hyunsu1234 <cookirihy1@gmail.com>
2023-02-24 23:53:21 +0900

    updated

--- js/scriptswiper.js
+++ js/scriptswiper.js
@@ -1,44 +1,45 @@
 const swiper = new Swiper('.wrap', {
-    direction: "horizontal",
+  direction: "horizontal",
+  // loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  spaceBetween: 0,
+  slidesPerView: "auto",
+  grabCursor: true,
+  centeredSlides: true,
+  speed: 1000,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: -100,
+    depth: 400,
+    modifier: 1,
+    slideShadows: false,
+  },
+  autoplay: {
+    delay: 1000, //시간설정
+    disableOnInteraction: false,//false-스와이프 후 자동재생
     loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'fraction'
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    spaceBetween: 0,
-    slidesPerView: "auto",
-    grabCursor: true,
-    centeredSlides: true,
-    speed: 1000,
-    effect: "coverflow",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: -100,
-      depth: 400,
-      modifier: 1,
-      slideShadows: false,
-    },
-    autoplay: {
-      delay: 1000,
-      disableOnInteraction: true
-    }
-  });
+  }
+});
 
-  // .btnStart 요소를 찾아서 btnStart에 저장
-  const btnStart = document.querySelector(".btnStart");
-  // .btnStop 요소를 찾아서 btnStop에 저장
-  const btnStop = document.querySelector(".btnStop");
+// .btnStart 요소를 찾아서 btnStart에 저장
+const btnStart = document.querySelector(".btnStart");
+// .btnStop 요소를 찾아서 btnStop에 저장
+const btnStop = document.querySelector(".btnStop");
 
-  //btnStart버튼을 클릭시 자동롤링 시작
-  btnStart.addEventListener("click", () => {
-    swiper.autoplay.start();
-  });
+//btnStart버튼을 클릭시 자동롤링 시작
+btnStart.addEventListener("click", () => {
+  swiper.autoplay.start();
+});
 
-  //btnStart버튼을 클릭시 자동롤링 정지
-  btnStop.addEventListener("click", () => {
-    swiper.autoplay.stop();
-  });
+//btnStart버튼을 클릭시 자동롤링 정지
+btnStop.addEventListener("click", () => {
+  swiper.autoplay.stop();
+});
